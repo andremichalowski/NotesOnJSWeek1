c8b120d896f08b37bf1fca62384c3b52113e7a3d
amichalo421 <andre.michalowski@gmail.com>
2019-10-15 14:59:57 +0200

    objectsFixesAdded

--- JS Week 1 Review/Day 1/Objects1.js
+++ JS Week 1 Review/Day 1/Objects1.js
@@ -14,11 +14,21 @@
             return (`Hello, my name is ${antonietta.name}`);
         },
         speakWithParameter: (parameter) => {
-            return (`Sometimes I ask myself is ${antonietta.name} ${(parameter)}`);
+            return (`Sometimes I ask myself is ${antonietta.name} ${parameter}`);
         },
         speakWithThisKeyword: function () {
             return (`Then I remember I have an email which is: ${this.email}`);
-        }
+        },
+        child: {
+            id: 4,
+            name: "Gannie",
+            email: "gmartinson3@illinois.edu",
+            gender: "M",
+            speakReassurance: (param) => {
+                return (`Mom...you ${antonietta.name} are a ${param}...we are both ${param}s`);
+            },
+        },
+
     }
 
     //2. Logging keys to elicit values:
@@ -27,27 +37,22 @@
         // or w/bracket notation
         console.log(antonietta['gender']);
 
-    //3. Loggin functions:
+    //3. Login functions:
 
         console.log(antonietta.multiplyNums(3,2));
         // or with reference
         console.log(antonietta.speakWithReference()); 
         // or with parameter reference
-        console.log(antonietta.speakWithParameter('ok')); //<<<(Don't forget your strings)
+        console.log(antonietta.speakWithParameter('real')); //<<<(Don't forget your strings)
          // or with "this" keyword
         console.log(antonietta.speakWithThisKeyword());
+
+    //4. Calling from nested objects (children):
+
+        console.log(antonietta.child.speakReassurance('robot'));
+
+
         
-
-    
-
-    
-
-        
-
-
-    
-
-
 
 
 

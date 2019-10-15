a592076d3938edd0976a04e8a248e6db2368f42d
amichalo421 <andre.michalowski@gmail.com>
2019-10-15 10:26:11 +0200

    initialCommit

---
+++ JS Week 1 Review/Day 1/Objects1.js
@@ -0,0 +1,22 @@
+//OBJECTS
+
+    //1. Making an object
+
+    const antonietta = {
+        id: 5,
+        name: "Antonietta",
+        email: "adaine5@samsung.com",
+        gender: "F",
+        multiplyNums: (num1, num2) => {
+        console.log(num1*num2);
+        }
+    }
+
+//2. Logging keys to elicit values
+
+
+
+
+/////------------------------------------QUESTIONS 1----------------------------/////
+
+How to delete a branch you made accidentally and then get back to previous branch
